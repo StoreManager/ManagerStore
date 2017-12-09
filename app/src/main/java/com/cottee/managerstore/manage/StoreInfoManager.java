880d2ac57115f163ef648c865318bb4507bbfcf4
997296384 <997296384@qq.com>
2017-12-09 16:04:40 +0800

    changeStoreInformationManager

--- app/src/main/java/com/cottee/managerstore/manage/StoreInfoManager.java
+++ app/src/main/java/com/cottee/managerstore/manage/StoreInfoManager.java
@@ -13,61 +13,47 @@ import java.util.List;
  */
 
 public class StoreInfoManager {
-  private Context context;
-  private List<String > list;
+    private Context context;
+
     public StoreInfoManager(Context context) {
         this.context = context;
     }
 
-    private String toast_storeName="请输入您店铺的名字";
-  private String toast_storeStyle="请选择您店铺的类型";
-  private String toast_storeAdress="请定位您店铺的地址";
-  private String toast_storePhoneNumber="请输入您店铺的联系电话";
-  private String toast_storeBusinessLicense="请上传您店铺的营业执照";
-  public boolean infoIsEmpty(String storeName,String storeStyle,String storeAddress,
-                             String storePhoneNumber,Drawable state)
-  {
-      if (TextUtils.isEmpty(storeName)) {
-          Toast.makeText(context, toast_storeName, Toast.LENGTH_SHORT).show();
-          return false;
-      }else if (TextUtils.isEmpty(storeStyle))
-      {
-          Toast.makeText(context, toast_storeStyle, Toast.LENGTH_SHORT).show();
-          return false;
-      }else if (TextUtils.isEmpty(storeAddress))
-      {
-          Toast.makeText(context, toast_storeAdress, Toast.LENGTH_SHORT).show();
-          return false;
-      }else if (TextUtils.isEmpty(storePhoneNumber))
-      {
-          Toast.makeText(context, toast_storePhoneNumber, Toast.LENGTH_SHORT).show();
-          return false;
-      }
-      else if (!TextUtils.isEmpty(storePhoneNumber)&&!isMobileNo(storePhoneNumber))
-      {
-          Toast.makeText(context, "电话号码不正确", Toast.LENGTH_SHORT).show();
-      }
-          else if (state==null){
-          Toast.makeText(context, toast_storeBusinessLicense, Toast.LENGTH_SHORT).show();
-          return false;
-      }
-      new ArrayList<String >();
-      for (int i=0;i<list.size();i++)
-      {
-          list.add("AA"+i);
-      }
-      Toast.makeText(context, "完成信息填写！", Toast.LENGTH_SHORT).show();
+    private String toast_storeName = "请输入您店铺的名字";
+    private String toast_storeStyle = "请选择您店铺的类型";
+    private String toast_storeAdress = "请定位您店铺的地址";
+    private String toast_storePhoneNumber = "请输入您店铺的联系电话";
+    private String toast_storeBusinessLicense = "请上传您店铺的营业执照";
 
-      return true;
-  }
+    public boolean infoIsEmpty(String storeName, String storeStyle, String storeAddress,
+                               String storePhoneNumber, Drawable state) {
+        if (TextUtils.isEmpty( storeName )) {
+            Toast.makeText( context, toast_storeName, Toast.LENGTH_SHORT ).show();
+            return false;
+        } else if (TextUtils.isEmpty( storeStyle )) {
+            Toast.makeText( context, toast_storeStyle, Toast.LENGTH_SHORT ).show();
+            return false;
+        } else if (TextUtils.isEmpty( storeAddress )) {
+            Toast.makeText( context, toast_storeAdress, Toast.LENGTH_SHORT ).show();
+            return false;
+        } else if (TextUtils.isEmpty( storePhoneNumber )) {
+            Toast.makeText( context, toast_storePhoneNumber, Toast.LENGTH_SHORT ).show();
+            return false;
+        } else if (!TextUtils.isEmpty( storePhoneNumber ) && !isMobileNo( storePhoneNumber )) {
+            Toast.makeText( context, "电话号码不正确", Toast.LENGTH_SHORT ).show();
+        } else if (state == null) {
+            Toast.makeText( context, toast_storeBusinessLicense, Toast.LENGTH_SHORT ).show();
+            return false;
+        }
+        return true;
+    }
 
-    public static  boolean isMobileNo(String mobileNums)
-    {
-        String telRegex="[1][3587]\\d{9}";
-        if(TextUtils.isEmpty(mobileNums))
-            return  false;
+    public static boolean isMobileNo(String mobileNums) {
+        String telRegex = "[1][3587]\\d{9}";
+        if (TextUtils.isEmpty( mobileNums ))
+            return false;
 
         else
-            return mobileNums.matches(telRegex);
+            return mobileNums.matches( telRegex );
     }
 }
