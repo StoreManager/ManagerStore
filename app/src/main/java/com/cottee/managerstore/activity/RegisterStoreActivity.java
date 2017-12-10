9393ba1529f0e974d541b48edaaed48e8e1b64b0
myt <1151803550@qq.com>
2017-12-10 12:36:50 +0800

    update BackKey of Activity

--- app/src/main/java/com/cottee/managerstore/activity/RegisterStoreActivity.java
+++ app/src/main/java/com/cottee/managerstore/activity/RegisterStoreActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -75,5 +76,20 @@ public class RegisterStoreActivity extends Activity {
         startActivity(intent);
         overridePendingTransition(R.anim.right_in, R.anim.left_out);
     }
+
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent home = new Intent(Intent.ACTION_MAIN);
+            home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            home.addCategory(Intent.CATEGORY_HOME);
+            startActivity(home);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
     
