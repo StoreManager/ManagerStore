60b63fe43d3b8ae0a74a4ab1d531840c1e52b223
997296384 <997296384@qq.com>
2017-12-10 18:47:22 +0800

    StoreInfo

---
+++ app/src/main/java/com/cottee/managerstore/bean/StoreInfo.java
@@ -0,0 +1,118 @@
+package com.cottee.managerstore.bean;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+/**
+ * Created by user on 2017/12/10.
+ */
+
+public class StoreInfo {
+    private String storeName;
+    private String storeStyle;
+    private String storeAddress;
+    private String storePhoneNumber;
+    private String minimumPay;//人均消费
+    private String autograph;//个性签名
+    private boolean isPobulish;//是否发布
+    private boolean isOpen;//是否打烊
+    private Bitmap mainCover;//封面图片
+    private List<Bitmap> bitmapList;//其他图片
+
+    public StoreInfo() {
+    }
+
+    public StoreInfo(String storeName, String storeStyle, String storeAddress, String storePhoneNumber, String minimumPay, String autograph, boolean isPobulish, boolean isOpen, Bitmap mainCover, List<Bitmap> bitmapList) {
+        this.storeName = storeName;
+        this.storeStyle = storeStyle;
+        this.storeAddress = storeAddress;
+        this.storePhoneNumber = storePhoneNumber;
+        this.minimumPay = minimumPay;
+        this.autograph = autograph;
+        this.isPobulish = isPobulish;
+        this.isOpen = isOpen;
+        this.mainCover = mainCover;
+        this.bitmapList = bitmapList;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreStyle() {
+        return storeStyle;
+    }
+
+    public void setStoreStyle(String storeStyle) {
+        this.storeStyle = storeStyle;
+    }
+
+    public String getStoreAddress() {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public String getStorePhoneNumber() {
+        return storePhoneNumber;
+    }
+
+    public void setStorePhoneNumber(String storePhoneNumber) {
+        this.storePhoneNumber = storePhoneNumber;
+    }
+
+    public String getMinimumPay() {
+        return minimumPay;
+    }
+
+    public void setMinimumPay(String minimumPay) {
+        this.minimumPay = minimumPay;
+    }
+
+    public String getAutograph() {
+        return autograph;
+    }
+
+    public void setAutograph(String autograph) {
+        this.autograph = autograph;
+    }
+
+    public boolean isPobulish() {
+        return isPobulish;
+    }
+
+    public void setPobulish(boolean pobulish) {
+        isPobulish = pobulish;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public Bitmap getMainCover() {
+        return mainCover;
+    }
+
+    public void setMainCover(Bitmap mainCover) {
+        this.mainCover = mainCover;
+    }
+
+    public List<Bitmap> getBitmapList() {
+        return bitmapList;
+    }
+
+    public void setBitmapList(List<Bitmap> bitmapList) {
+        this.bitmapList = bitmapList;
+    }
+}
