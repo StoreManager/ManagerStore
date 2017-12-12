a8bd4125e68680ec8ae52b1028a982ec25f079dd
997296384 <997296384@qq.com>
2017-12-12 15:41:11 +0800

    storeManagerActivity

--- app/src/main/java/com/cottee/managerstore/activity/StoreManagerActivity.java
+++ app/src/main/java/com/cottee/managerstore/activity/StoreManagerActivity.java
@@ -1,15 +1,20 @@
 package com.cottee.managerstore.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.cottee.managerstore.R;
 
-public class StoreManagerActivity extends Activity {
+public class StoreManagerActivity extends Activity implements View.OnClickListener {
 
     private TextView tv_storename_manager;
+    private Button btn_storeManager;
+    public Context mContext = StoreManagerActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,26 @@ public class StoreManagerActivity extends Activity {
         String storename = intent.getStringExtra( "storename" );
 
 
+        findView();
+        tv_storename_manager.setText( storename + "管理" );
+    }
+
+    private void findView() {
         tv_storename_manager = (TextView) findViewById( R.id.tv_storename_manager );
-        tv_storename_manager.setText( storename+"管理" );
+        btn_storeManager = (Button) findViewById( R.id.btn_storeManager );
+        btn_storeManager.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_storeManager:
+                Intent intent = new Intent( this, DetialInfomation.class );
+                startActivity( intent );
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
