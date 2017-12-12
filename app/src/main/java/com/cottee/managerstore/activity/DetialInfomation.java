a34bd7809663971bcbf6b17f58b988b19aff5d5b
997296384 <997296384@qq.com>
2017-12-12 15:26:41 +0800

    storeManagerActivity

---
+++ app/src/main/java/com/cottee/managerstore/activity/DetialInfomation.java
@@ -0,0 +1,18 @@
+package com.cottee.managerstore.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cottee.managerstore.R;
+
+/**
+ * Created by user on 2017/12/12.
+ */
+
+public class DetialInfomation extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_detialinfomation );
+    }
+}
