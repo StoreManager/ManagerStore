8c501b6bfad64abd9ff7217a378ea6a15fbf7a87
xiaowen <1581593923@qq.com>
2017-12-13 11:34:08 +0800

    modify sroremanager

--- app/src/main/java/com/cottee/managerstore/activity/RegisterStoreActivity.java
+++ app/src/main/java/com/cottee/managerstore/activity/RegisterStoreActivity.java
@@ -8,11 +8,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,8 +30,15 @@ import com.cottee.managerstore.bean.StoreInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RegisterStoreActivity extends Activity {
 
+public class RegisterStoreActivity extends AppCompatActivity {
+    private Toolbar tl_custom;
+    private DrawerLayout dl_left;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private LinearLayout linear_changePassword;
+    private LinearLayout linear_exitLogin;
+    private LinearLayout linear_helpCenter;
+    private LinearLayout linear_feedbackProblem;
     private ListView lv_registerStore;
 
     private StoreListviewAdapter storeListviewAdapter;
@@ -37,27 +49,47 @@ public class RegisterStoreActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        requestWindowFeature( Window.FEATURE_NO_TITLE );
+//        requestWindowFeature( Window.FEATURE_NO_TITLE );
         setContentView( R.layout.activity_registerstore );
-        lv_registerStore = findViewById( R.id.lv_registerStore );
-        Button button = (Button) findViewById( R.id.btn_user_exit );
+        initView();
+        initEvent();
+        tl_custom.setTitle("Sweet商铺管理系统");//设置Toolbar标题
+        tl_custom.setBackgroundColor(getResources().getColor(R.color.purplishblue));
+        tl_custom.setTitleTextColor(getResources().getColor(R.color.white)); //设置标题颜色
+        setSupportActionBar(tl_custom);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, dl_left,
+                tl_custom,R.string.drawer_open,R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        dl_left.setDrawerListener(mDrawerToggle);
+//        Button button = (Button) findViewById( R.id.btn_user_exit );
         tv_nostore = (TextView) findViewById( R.id.tv_noStore );
         getStoreList();
-        button.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences sp = getSharedPreferences( "userLogin", Context.MODE_PRIVATE );
-                SharedPreferences.Editor editor = sp.edit();
-                editor.clear();
-                editor.commit();
-
-                Intent intent = new Intent( RegisterStoreActivity.this, BossLoginActivity.class );
-                startActivity( intent );
-                finish();
-
-
-            }
-        } );
+//        button.setOnClickListener( new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                SharedPreferences sp = getSharedPreferences( "userLogin", Context.MODE_PRIVATE );
+//                SharedPreferences.Editor editor = sp.edit();
+//                editor.clear();
+//                editor.commit();
+//
+//                Intent intent = new Intent( RegisterStoreActivity.this, BossLoginActivity.class );
+//                startActivity( intent );
+//                finish();
+//
+//
+//            }
+//        } );
         lv_registerStore.setOnItemClickListener( new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int position, long l) {
@@ -82,7 +114,57 @@ public class RegisterStoreActivity extends Activity {
             }
         } );
     }
+    private void initView() {
+        lv_registerStore = findViewById(R.id.lv_registerStore);
+        tl_custom = findViewById(R.id.tl_custom);
+        dl_left = findViewById(R.id.dl_left);
+        linear_changePassword = findViewById(R.id.linear_changePassword);
+        linear_exitLogin = findViewById(R.id.linear_exitLogin);
+        linear_helpCenter = findViewById(R.id.linear_helpCenter);
+        linear_feedbackProblem = findViewById(R.id.linear_feedbackProblem);
+    }
+    private void initEvent()
+    {
+//       修改密码
+        linear_changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterStoreActivity.this, "修改密码！", Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+//       退出登录
+        linear_exitLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sp = getSharedPreferences("userLogin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.clear();
+                editor.commit();
 
+                Intent intent = new Intent(RegisterStoreActivity.this, BossLoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.left_in,R.anim.right_out);
+            }
+        });
+//    帮助中心
+        linear_helpCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterStoreActivity.this, "帮助中心", Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+//       问题反馈
+        linear_feedbackProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterStoreActivity.this, "问题反馈", Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+    }
     @Override
     protected void onResume() {
         super.onResume();
--- app/src/main/java/com/cottee/managerstore/activity/StoreManagerActivity.java
+++ app/src/main/java/com/cottee/managerstore/activity/StoreManagerActivity.java
@@ -1,41 +1,84 @@
 package com.cottee.managerstore.activity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cottee.managerstore.R;
+import com.cottee.managerstore.view.PressureButton;
+import com.cottee.managerstore.view.StoreStausPopupWindow;
 
-public class StoreManagerActivity extends Activity implements View.OnClickListener {
-
-    private TextView tv_storename_manager;
+public class StoreManagerActivity extends AppCompatActivity implements View.OnClickListener {
+    private Toolbar tl_custom;
+    private DrawerLayout dl_left;
+    private ActionBarDrawerToggle mDrawerToggle;
+//    private TextView tv_storename_manager;
     private Button btn_storeManager;
     public Context mContext = StoreManagerActivity.this;
     private TextView tv_storeManager;
+    private LinearLayout linear_changeStore;
+    private StoreStausPopupWindow storeStausPopupWindow;
+    private TextView tv_storeStatus;
+    private Button btn_releaseStore;
+    private PressureButton pbtn_order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_storemanager );
-
+        findView();
+        tl_custom.setTitle("");//设置Toolbar标题
+        tl_custom.setBackgroundColor(getResources().getColor(R.color.purplishblue));
+//        tl_custom.setTitleTextColor(getResources().getColor(R.color.white)); //设置标题颜色
+        setSupportActionBar(tl_custom);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, dl_left,
+                tl_custom,R.string.drawer_open,R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        dl_left.setDrawerListener(mDrawerToggle);
         Intent intent = getIntent();
         String storename = intent.getStringExtra( "storename" );
 
 
-        findView();
-        tv_storename_manager.setText( storename + "管理" );
+
+//        tv_storename_manager.setText( storename + "管理" );
     }
 
     private void findView() {
-        tv_storename_manager = (TextView) findViewById( R.id.tv_storename_manager );
+        pbtn_order = findViewById(R.id.pbtn_order);
+        btn_releaseStore = findViewById(R.id.btn_releaseStore);
+        tv_storeStatus = findViewById(R.id.tv_storeStatus);
+        tl_custom = findViewById(R.id.tl_custom);
+        dl_left = findViewById(R.id.dl_left);
+        linear_changeStore = findViewById(R.id.linear_changeStore);
+//        tv_storename_manager = (TextView) findViewById( R.id.tv_storename_manager );
         btn_storeManager = (Button) findViewById( R.id.btn_storeManager );
         btn_storeManager.setOnClickListener( this );
         tv_storeManager = (TextView) findViewById( R.id.tv_storeManager );
+        pbtn_order.setOnClickListener(this);
+        btn_releaseStore.setOnClickListener(this);
+        tv_storeStatus.setOnClickListener(this);
         tv_storeManager.setOnClickListener( this );
+        linear_changeStore.setOnClickListener(this);
     }
 
     @Override
@@ -47,8 +90,40 @@ public class StoreManagerActivity extends Activity implements View.OnClickListen
                 startActivity( intent );
                 finish();
                 break;
+            case R.id.linear_changeStore:
+                startActivity(new Intent(this,RegisterStoreActivity.class));
+                finish();
+                overridePendingTransition(R.anim.left_in,R.anim.right_out);
+                break;
+            case R.id.tv_Status:
+                storeStausPopupWindow = new
+                        StoreStausPopupWindow(this);
+                storeStausPopupWindow.getContentView().setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+
+
+                    @Override
+                    public void onFocusChange(View view, boolean hasFocus) {
+                        if (!hasFocus)
+                            storeStausPopupWindow.dismiss();
+
+                    }
+                });
+                //设置默认获取焦点
+                storeStausPopupWindow.setFocusable(true);
+//以某个控件的x和y的偏移量位置开始显示窗口
+                storeStausPopupWindow.showAsDropDown(tv_storeStatus, 0, 0);
+//如果窗口存在，则更新
+                storeStausPopupWindow.update();
+                break;
+            case R.id.btn_releaseStore:
+                Toast.makeText(mContext, "发布店铺", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pbtn_order:
+                Toast.makeText(mContext, "下单", Toast.LENGTH_SHORT).show();
             default:
                 break;
         }
     }
+
 }
