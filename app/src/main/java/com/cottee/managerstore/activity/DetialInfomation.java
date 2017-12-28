b77d044f2b293597dbb828f9d96283cd8b29a7ec
997296384 <997296384@qq.com>
2017-12-28 14:23:57 +0800

    backDialog

--- app/src/main/java/com/cottee/managerstore/activity/DetialInfomation.java
+++ app/src/main/java/com/cottee/managerstore/activity/DetialInfomation.java
@@ -1,24 +1,26 @@
 package com.cottee.managerstore.activity;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.bumptech.glide.Glide;
 import com.cottee.managerstore.R;
 import com.cottee.managerstore.bean.StoreInfo;
+import com.cottee.managerstore.utils.ToastUtils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by user on 2017/12/12.
@@ -29,8 +31,8 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
     private TextView tv_storeName;
     private Button btn_edit;
     private Button btn_save;
-    private TextView tv_time;
-    private EditText et_time;
+    private TextView tv_timeAM;
+    private EditText et_timeAM;
     private TextView tv_sign;
     private EditText et_sign;
     private TextView tv_money;
@@ -43,7 +45,8 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
     private ImageView iv_photo2;
     private ImageView iv_photo3;
     private String sign;
-    private String time;
+    private String timeAM;
+    private String timePM;
     private String money;
     private String phone;
     private StoreInfo storeInfo;
@@ -53,7 +56,9 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
     private String one;
     private String two;
     private String three;
-    public Context mContext=DetialInfomation.this;
+    public Context mContext = DetialInfomation.this;
+    private TextView tv_timePM;
+    private EditText et_timePM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,8 +76,12 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
         Glide.with( this ).load( one ).into( iv_photo1 );
         Glide.with( this ).load( two ).into( iv_photo2 );
         Glide.with( this ).load( three ).into( iv_photo3 );
+
+        String time = storeInfo.getBusiness_hours();
+        String[] split = time.split( "-" );
         tv_storeName.setText( storeInfo.getName() );
-        tv_time.setText( storeInfo.getBusiness_hours() );
+        tv_timeAM.setText( split[0] );
+        tv_timePM.setText( split[1] );
         tv_sign.setText( storeInfo.getIntroduce() );
         tv_phone.setText( storeInfo.getPhone() );
         tv_style.setText( "(" + storeInfo.getClassify() + ")" );
@@ -85,8 +94,11 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
         btn_edit = (Button) findViewById( R.id.btn_edit );
         btn_save = (Button) findViewById( R.id.btn_save );
 
-        tv_time = (TextView) findViewById( R.id.tv_time );
-        et_time = (EditText) findViewById( R.id.et_time );
+        tv_timeAM = (TextView) findViewById( R.id.tv_timeAM );
+        et_timeAM = (EditText) findViewById( R.id.et_timeAM );
+
+        tv_timePM = (TextView) findViewById( R.id.tv_timePM );
+        et_timePM = (EditText) findViewById( R.id.et_timePM );
 
         tv_sign = (TextView) findViewById( R.id.tv_sign );
         et_sign = (EditText) findViewById( R.id.et_sign );
@@ -108,21 +120,27 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
         iv_photo2.setOnClickListener( this );
         iv_photo3 = (ImageView) findViewById( R.id.iv_photo3 );
         iv_photo3.setOnClickListener( this );
+        et_timeAM.setOnClickListener( this );
+        et_timePM.setOnClickListener( this );
 
     }
 
     public void edit(View view) {
 
         sign = tv_sign.getText().toString().trim();
-        time = tv_time.getText().toString().trim();
+        timeAM = tv_timeAM.getText().toString().trim();
+        timePM = tv_timePM.getText().toString().trim();
         money = tv_money.getText().toString().trim();
         phone = tv_phone.getText().toString().trim();
 
         tv_sign.setVisibility( View.GONE );
         et_sign.setVisibility( View.VISIBLE );
 
-        tv_time.setVisibility( View.GONE );
-        et_time.setVisibility( View.VISIBLE );
+        tv_timeAM.setVisibility( View.GONE );
+        et_timeAM.setVisibility( View.VISIBLE );
+
+        tv_timePM.setVisibility( View.GONE );
+        et_timePM.setVisibility( View.VISIBLE );
 
         tv_money.setVisibility( View.GONE );
         et_money.setVisibility( View.VISIBLE );
@@ -134,7 +152,8 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
         btn_save.setVisibility( View.VISIBLE );
 
         et_sign.setText( sign );
-        et_time.setText( time );
+        et_timeAM.setText( timeAM );
+        et_timePM.setText( timePM );
         et_money.setText( money );
         et_phone.setText( phone );
 
@@ -142,53 +161,40 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
 
     public void save(View view) {
         sign = et_sign.getText().toString().trim();
-        time = et_time.getText().toString().trim();
+        timeAM = et_timeAM.getText().toString().trim();
+        timePM = et_timePM.getText().toString().trim();
         money = et_money.getText().toString().trim();
         phone = et_phone.getText().toString().trim();
 
-        et_sign.setVisibility( View.GONE );
-        tv_sign.setVisibility( View.VISIBLE );
+        boolean mobileNo = isMobileNo( phone );
+        if (mobileNo==true) {
+            et_sign.setVisibility( View.GONE );
+            tv_sign.setVisibility( View.VISIBLE );
 
-        et_time.setVisibility( View.GONE );
-        tv_time.setVisibility( View.VISIBLE );
+            et_timeAM.setVisibility( View.GONE );
+            tv_timeAM.setVisibility( View.VISIBLE );
 
-        et_money.setVisibility( View.GONE );
-        tv_money.setVisibility( View.VISIBLE );
+            et_timePM.setVisibility( View.GONE );
+            tv_timePM.setVisibility( View.VISIBLE );
 
-        et_phone.setVisibility( View.GONE );
-        tv_phone.setVisibility( View.VISIBLE );
+            et_money.setVisibility( View.GONE );
+            tv_money.setVisibility( View.VISIBLE );
 
-        btn_edit.setVisibility( View.VISIBLE );
-        btn_save.setVisibility( View.GONE );
+            et_phone.setVisibility( View.GONE );
+            tv_phone.setVisibility( View.VISIBLE );
 
-        tv_sign.setText( sign );
-        tv_time.setText( time );
-        tv_money.setText( money );
-        tv_phone.setText( phone );
-    }
+            btn_edit.setVisibility( View.VISIBLE );
+            btn_save.setVisibility( View.GONE );
 
-    public byte[] getImage(String path) throws Exception {
-        URL url = new URL( path );
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout( 5 * 1000 );
-        conn.setRequestMethod( "GET" );
-        InputStream inStream = conn.getInputStream();
-        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-            return readStream( inStream );
+            tv_sign.setText( sign );
+            tv_timeAM.setText( timeAM );
+            tv_timePM.setText( timePM );
+            tv_money.setText( money );
+            tv_phone.setText( phone );
+        } else {
+            ToastUtils.showToast( this,"电话号码输入有误哦" );
+            return;
         }
-        return null;
-    }
-
-    public static byte[] readStream(InputStream inStream) throws Exception {
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len = 0;
-        while ((len = inStream.read( buffer )) != -1) {
-            outStream.write( buffer, 0, len );
-        }
-        outStream.close();
-        inStream.close();
-        return outStream.toByteArray();
     }
 
     @Override
@@ -211,8 +217,39 @@ public class DetialInfomation extends Activity implements View.OnClickListener {
                 intent.putExtra( "photo_url", three );
                 startActivity( intent );
                 break;
+            case R.id.et_timeAM:
+                showTimePickerDialog( et_timeAM );
+                break;
+            case R.id.et_timePM:
+                showTimePickerDialog( et_timePM );
+                break;
             default:
                 break;
         }
+    }
+
+    private void showTimePickerDialog(final EditText editText) {
+        final Calendar calendar = Calendar.getInstance();
+        TimePickerDialog dialog = new TimePickerDialog( this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        calendar.set( Calendar.HOUR, i );
+                        calendar.set( Calendar.MINUTE, i1 );
+                        SimpleDateFormat format = new SimpleDateFormat( "HH:mm" );
+                        String s = format.format( calendar.getTime() );
+                        editText.setText( s );
+                    }
+                }, calendar.get( Calendar.HOUR ), calendar.get( Calendar.MINUTE ), true );
+        dialog.show();
+    }
+
+    public static boolean isMobileNo(String mobileNums) {
+        String telRegex = "[1][3587]\\d{9}";
+        if (TextUtils.isEmpty( mobileNums ))
+            return false;
+
+        else
+            return mobileNums.matches( telRegex );
     }
 }
