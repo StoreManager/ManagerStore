29b8f63af8c374fbc9aed00d6c5512a09770344b
myt <1151803550@qq.com>
2018-02-23 18:41:59 +0800

    标题栏优化

--- app/src/main/java/com/cottee/managerstore/activity1/EmployeeManageActivity.java
+++ app/src/main/java/com/cottee/managerstore/activity1/EmployeeManageActivity.java
@@ -7,6 +7,7 @@ import android.widget.ListView;
 
 import com.cottee.managerstore.R;
 import com.cottee.managerstore.adapter.EmployeeManageAdapter;
+import com.cottee.managerstore.widget.Title;
 
 /**
  * Created by Administrator on 2018/1/2.
@@ -15,18 +16,43 @@ import com.cottee.managerstore.adapter.EmployeeManageAdapter;
 public class EmployeeManageActivity extends Activity {
 
     private ListView lv_employee_manage_information;
+    private Title title;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_manage);
-
-        initview();
+        initTitle();
+        initView();
         EmployeeManageAdapter adapter = new EmployeeManageAdapter(this);
         lv_employee_manage_information.setAdapter(adapter);
     }
 
-    private void initview() {
+    private void initView() {
         lv_employee_manage_information = (ListView) findViewById(R.id.lv_employee_manage_information);
+    }
+    private void initTitle(){
+        title = (Title)findViewById(R.id.title);
+        title.setTitleNameStr("员工管理");
+        title.setTheme(Title.TitleTheme.THEME_TRANSLATE);
+        title.mSetButtonInfo(new Title.ButtonInfo(true, Title
+                .BUTTON_LEFT, R.mipmap.back_2x,null
+        ));
+        //可加button1
+        title.mSetButtonInfo(new Title.ButtonInfo(true, Title
+                .BUTTON_RIGHT1, 0,
+                "me"));
+        title.setOnTitleButtonClickListener(new Title.OnTitleButtonClickListener() {
+            @Override
+            public void onClick(int id, Title.ButtonViewHolder viewHolder) {
+                switch (id) {
+                    case Title.BUTTON_RIGHT1:
+                        break;
+                    case Title.BUTTON_LEFT:
+                        break;
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cottee/managerstore/widget/Title.java
+++ app/src/main/java/com/cottee/managerstore/widget/Title.java
@@ -10,21 +10,11 @@ import android.widget.TextView;
 
 import com.cottee.managerstore.R;
 
+
 /**
- * ==================================================
- * Company: Black and White Universal(c) 2017
- * *
- * Author: Kevin
- * *
- * Data: 2017/7/15 16:34
- * *
- * Description:
- * ==================================================
+ * Created by lenovo on 2016/2/22.
  */
-
-
 public class Title extends RelativeLayout {
-
 
     private RelativeLayout titleCustomLayoutAear = null;//自定义View区
     private View titleCustomView = null;//用户设置的自定义View
@@ -32,7 +22,8 @@ public class Title extends RelativeLayout {
     private TextView titleName = null;//标题
     private String titleNameStr = null;
     private boolean isShowTitle = true;
-
+    public static final int GONE = 8;
+    public static final int VISIBLE = 0;
     private View titleDivider = null;//分隔线
     private boolean isShowDivider = true;//是否显示分隔线
 
@@ -42,72 +33,13 @@ public class Title extends RelativeLayout {
 
     private OnTitleButtonClickListener onTitleButtonClickListener;
 
-
     public Title(Context context, AttributeSet attrs) {
-        super(context, attrs);//继承自RelativeLayout,必要
-    }
-
-
-    public void setTitleNameRes(int resId){//???现在没用
-        this.titleNameStr = getResources().getString(resId);
-        if(null != titleName){
-            titleName.setText(titleNameStr);
-        }
-    }
-
-    public void setTitleNameStr(String name){
-        this.titleNameStr = name;
-        if(null != titleName){
-            titleName.setText(titleNameStr);
-        }
-    }
-
-    public void showTitle(boolean showTitle){
-        this.isShowTitle = showTitle;
-        if(isShowTitle){
-            titleName.setVisibility(View.VISIBLE);
-        } else {
-            titleName.setVisibility(View.GONE);
-        }
-    }
-
-    public void setShowDivider(boolean showDivider) {
-        isShowDivider = showDivider;
-        refreshDividerStatus();//判断分割线，为什么不写if判断
-    }
-
-    public void setOnTitleButtonClickListener(OnTitleButtonClickListener onTitleButtonClickListener) {
-        this.onTitleButtonClickListener = onTitleButtonClickListener;
-    }
-
-    public void mSetButtonInfo(ButtonInfo buttonInfo){//对按钮的选择
-        if(buttonInfo == null){
-            return;
-        }
-        switch (buttonInfo.id){
-            case BUTTON_LEFT:
-                buttonHolderLeft.buttonInfo =  buttonInfo;
-                break;
-
-            case BUTTON_RIGHT1:
-                buttonHolderRight1.buttonInfo =  buttonInfo;
-                break;
-
-            case BUTTON_RIGHT2:
-                buttonHolderRight2.buttonInfo =  buttonInfo;
-                break;
-            default:
-                return;
-        }
-        refreshButtonShow(new ButtonViewHolder[]{buttonHolderLeft, buttonHolderRight1, buttonHolderRight2});
-    //刷新按钮
+        super(context, attrs);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
-
         //自定义View区
         titleCustomLayoutAear = (RelativeLayout) findViewById(R.id.titleCustomLayoutAear);
         if(titleCustomView != null){
@@ -148,74 +80,170 @@ public class Title extends RelativeLayout {
 
         refreshButtonShow(new ButtonViewHolder[]{buttonHolderLeft, buttonHolderRight1, buttonHolderRight2});
 
-        refreshTheme();
+        refreshTheme(TitleTheme.THEME_LIGHT);
 
-        if(null != onFinishCustomLayout){//view监控
+        if(null != onFinishCustomLayout){
             onFinishCustomLayout.onFinishCustomLayout(titleCustomLayoutAear, titleCustomView);
         }
     }
 
-//ViewHolder就是一个持有者的类，他里面一般没有方法，只有属性，作用就是一个临时的储存器，
-// 把你getView方法中每次返回的View存起来，可以下次再用。
-// 这样做的好处就是不必每次都到布局文件中去拿到你的View，提高了效率。不能一次都出来，现在写了5个按钮，
-   // 拿的时候要new button出来，Viewhold不用new了，
-
-    public class ButtonViewHolder{
-
-        public ButtonViewHolder(boolean isShow, int id){
-            buttonInfo = new ButtonInfo(isShow, id);
+    public void setTitleNameRes(int resId){
+        this.titleNameStr = getResources().getString(resId);
+        if(null != titleName){
+            titleName.setText(titleNameStr);
         }
+    }
 
-        public View rootView;
-        public TextView text;
-        public ImageView image;
+    public void setTitleNameStr(String name){
+        this.titleNameStr = name;
+        if(null != titleName){
+            titleName.setText(titleNameStr);
+        }
+    }
 
-        private ButtonInfo buttonInfo;
+    public void showTitle(boolean showTitle){
+        this.isShowTitle = showTitle;
+        if(isShowTitle){
+            titleName.setVisibility(View.VISIBLE);
+        } else {
+            titleName.setVisibility(View.GONE);
+        }
+    }
 
-        public void initOnClick(){
-            rootView.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(null != onTitleButtonClickListener){
-                        onTitleButtonClickListener.onClick(buttonInfo.id, ButtonViewHolder.this);
-                    }
+    /**
+     * 添加自定义View，将会替换前一个
+     * @param customView
+     */
+    public void addCustomView(View customView){
+        titleCustomView = customView;
+        if(titleCustomLayoutAear != null){
+            if(customView == null){
+                titleCustomLayoutAear.removeAllViews();
+                titleCustomLayoutAear.setVisibility(View.GONE);
+            }else{
+                titleCustomLayoutAear.setVisibility(View.VISIBLE);
+                if(customView != titleCustomLayoutAear.getChildAt(0)){
+                    titleCustomLayoutAear.removeAllViews();
+                    titleCustomLayoutAear.addView(customView);
                 }
-            });
+            }
         }
     }
 
+    public void setOnFinishCustomLayout(OnFinishCustomLayout onFinishCustomLayout) {
+        this.onFinishCustomLayout = onFinishCustomLayout;
+    }
 
-//对按钮显示内用的选择
-    public static final int  BUTTON_LEFT   = 1;
-    public static final int  BUTTON_RIGHT1 = 2;
-    public static final int  BUTTON_RIGHT2 = 3;
+    /**
+     * 返回指标标识的View
+     * @param id
+     * @return
+     */
+    public ButtonViewHolder getViewHolder(int id){
+        switch (id){
+            case BUTTON_LEFT:
+                return buttonHolderLeft;
 
-    public static class ButtonInfo{
+            case BUTTON_RIGHT1:
+                return buttonHolderRight1;
 
-
-        public boolean isShow;
-        public int id = BUTTON_LEFT;
-        public int iconRes = 0;//如果为0表示图标不显示
-        public String name = null;//如果为null表示文字不显示
-        public boolean isOpen = false;//支持状态：展开或者关闭，用于按钮可能有状的情况
-
-        public ButtonInfo(boolean isShow, int id){
-            this.isShow = isShow;
-            this.id = id;
+            case BUTTON_RIGHT2:
+                return buttonHolderRight2;
         }
-
-        public ButtonInfo(boolean isShow, int id, int icRes, String name){
-            this.isShow = isShow;
-            this.id = id;
-            this.iconRes = icRes;
-            this.name = name;
-        }
-
-
+        return null;
+    }
+    public enum TitleTheme {
+        THEME_TRANSLATE,//主题_透明白分割线
+        THEME_LIGHT,//主题_明亮，背景是浅色
+        THEME_DARK,//主题_黑色，背景是深色
+        THEME_TRANSLATE_NODIVIDER,//主题_透明无分割线
+        THEME_TRANSLATE_GRAYDIVIDER;//主题_灰无分割线
     }
 
 
+    /**
+     * 设置主题
+     * @param titleTheme
+     */
+    public void setTheme(TitleTheme titleTheme){
+        refreshTheme(titleTheme);
+    }
 
+    private void refreshTheme(TitleTheme titleTheme){
+        switch (titleTheme){
+            case THEME_TRANSLATE:
+                if(titleName != null){
+                    titleName.setTextColor(Color.WHITE);
+                }
+                buttonHolderLeft.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight1.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight2.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+
+                titleDivider.setBackgroundColor(Color.WHITE);
+                this.setBackgroundResource(R.color.purplishblue);
+                break;
+
+            case THEME_TRANSLATE_GRAYDIVIDER:
+                if(titleName != null){
+                    titleName.setTextColor(Color.WHITE);
+                }
+                buttonHolderLeft.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight1.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight2.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                titleDivider.setBackgroundResource(R.color.grayDividerDark);
+
+                this.setBackgroundResource(android.R.color.transparent);
+                break;
+            case THEME_LIGHT:
+                if(titleName != null){
+                    titleName.setTextColor(getResources().getColor(R.color.white));
+                }
+                buttonHolderLeft.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themelight));
+                buttonHolderRight1.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themelight));
+                buttonHolderRight2.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themelight));
+                titleDivider.setBackgroundResource(R.color.grayDividerDark);
+                this.setBackgroundResource(R.color.white);
+                break;
+
+            case THEME_DARK:
+                if(titleName != null){
+                    titleName.setTextColor(Color.WHITE);
+                }
+                buttonHolderLeft.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight1.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight2.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+
+                titleDivider.setBackgroundColor(Color.WHITE);
+                this.setBackgroundResource(android.R.color.transparent);
+                break;
+            case THEME_TRANSLATE_NODIVIDER:
+                if(titleName != null){
+                    titleName.setTextColor(Color.WHITE);
+                }
+                buttonHolderLeft.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight1.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+                buttonHolderRight2.text.setTextColor(getResources().getColorStateList(R.color.selector_titletext_themetransport));
+
+                titleDivider.setBackgroundResource(android.R.color.transparent);
+                this.setBackgroundResource(android.R.color.transparent);
+                break;
+        }
+
+        refreshDividerStatus();
+    }
+
+    public void setShowDivider(boolean showDivider) {
+        isShowDivider = showDivider;
+        refreshDividerStatus();
+    }
+
+    private void refreshDividerStatus(){
+        if(isShowDivider){
+            titleDivider.setVisibility(View.VISIBLE);
+        }else{
+            titleDivider.setVisibility(View.GONE);
+        }
+    }
 
     /**
      * 刷新按钮显示
@@ -247,73 +275,89 @@ public class Title extends RelativeLayout {
         }
     }
 
-
-
-
-    public static final int THEME_TRANSLATE = 1;//主题_透明
-    public static final int THEME_LIGHT     = 2;//主题_明亮，背景是浅色
-    public static final int THEME_DARK      = 3;//主题_黑色，背景是深色
-    private int theme  = THEME_LIGHT;
-    /**
-     * 设置主题
-     * @param theme
-     */
-    public void setTheme(int theme){
-        this.theme = theme;
-        refreshTheme();
+    public void setOnTitleButtonClickListener(OnTitleButtonClickListener onTitleButtonClickListener) {
+        this.onTitleButtonClickListener = onTitleButtonClickListener;
     }
-    private void refreshTheme(){
-        switch (theme){
-            case THEME_TRANSLATE:
-                if(titleName != null){
-                    titleName.setTextColor(Color.TRANSPARENT);
 
-                }
-                buttonHolderLeft.text.setTextColor(Color.WHITE);
-                buttonHolderRight1.text.setTextColor(Color.WHITE);
-                buttonHolderRight2.text.setTextColor(Color.WHITE);
-                titleDivider.setBackgroundColor(Color.WHITE);
+    public void mSetButtonInfo(ButtonInfo buttonInfo){
+        if(buttonInfo == null){
+            return;
+        }
+        switch (buttonInfo.id){
+            case BUTTON_LEFT:
+                buttonHolderLeft.buttonInfo =  buttonInfo;
                 break;
 
-            case THEME_LIGHT:
-                if(titleName != null){
-                    titleName.setTextColor(Color.BLACK);
-                }
-                buttonHolderLeft.text.setTextColor(Color.BLACK);
-                buttonHolderRight1.text.setTextColor(Color.BLACK);
-                buttonHolderRight2.text.setTextColor(Color.BLACK);
-                titleDivider.setBackgroundResource(android.R.color.darker_gray);
+            case BUTTON_RIGHT1:
+                buttonHolderRight1.buttonInfo =  buttonInfo;
                 break;
 
-            case THEME_DARK:
-                if(titleName != null){
-                    titleName.setTextColor(Color.WHITE);
-                }
-                buttonHolderLeft.text.setTextColor(Color.WHITE);
-                buttonHolderRight1.text.setTextColor(Color.WHITE);
-                buttonHolderRight2.text.setTextColor(Color.WHITE);
-                titleDivider.setBackgroundColor(Color.WHITE);
+            case BUTTON_RIGHT2:
+                buttonHolderRight2.buttonInfo =  buttonInfo;
                 break;
+            default:
+                return;
+        }
+        refreshButtonShow(new ButtonViewHolder[]{buttonHolderLeft, buttonHolderRight1, buttonHolderRight2});
+    }
+
+    public static final int  BUTTON_LEFT   = 1;
+    public static final int  BUTTON_RIGHT1 = 2;
+    public static final int  BUTTON_RIGHT2 = 3;
+
+    public static class ButtonInfo{
+
+        public ButtonInfo(boolean isShow, int id){
+            this.isShow = isShow;
+            this.id = id;
         }
 
-        refreshDividerStatus();//分割线刷了两次
-    }
-    //刷新分割线
+        public ButtonInfo(boolean isShow, int id, int icRes, String name){
+            this.isShow = isShow;
+            this.id = id;
+            this.iconRes = icRes;
+            this.name = name;
+        }
 
-    private void refreshDividerStatus(){
-        if(isShowDivider){
-            titleDivider.setVisibility(View.VISIBLE);
-        }else{
-            titleDivider.setVisibility(View.GONE);
+        public boolean isShow;
+        public int id = BUTTON_LEFT;
+        public int iconRes = 0;//如果为0表示图标不显示
+        public String name = null;//如果为null表示文字不显示
+        public boolean isOpen = false;//支持状态：展开或者关闭，用于按钮可能有状的情况
+    }
+
+    public class ButtonViewHolder{
+
+        public ButtonViewHolder(boolean isShow, int id){
+            buttonInfo = new ButtonInfo(isShow, id);
+        }
+
+        public View rootView;
+        public TextView text;
+        public ImageView image;
+
+        private ButtonInfo buttonInfo;
+
+        public void initOnClick(){
+            rootView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(null != onTitleButtonClickListener){
+                        onTitleButtonClickListener.onClick(buttonInfo.id, ButtonViewHolder.this);
+                    }
+                }
+            });
         }
     }
-//拓展内容，
+
     public interface OnTitleButtonClickListener{
-        public void onClick(int id, ButtonViewHolder viewHolder);
-
+        void onClick(int id, ButtonViewHolder viewHolder);
     }
 
-    public interface OnFinishCustomLayout{//关于最后阶段  如果设置自定义View可以使用此监听
-        public void onFinishCustomLayout(View customViewLayout, View customView);
+
+    public interface OnFinishCustomLayout{
+        void onFinishCustomLayout(View customViewLayout, View customView);
     }
+
+
 }
