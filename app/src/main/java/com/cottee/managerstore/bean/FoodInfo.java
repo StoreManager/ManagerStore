ed0f6ea2b356c229088c0b93ae0d03a299dd0cb1
xiaowen <1581593923@qq.com>
2018-04-24 00:34:59 +0800

    ooo

---
+++ app/src/main/java/com/cottee/managerstore/bean/FoodInfo.java
@@ -0,0 +1,35 @@
+package com.cottee.managerstore.bean;
+
+/**
+ * Created by 37444 on 2018/3/31.
+ */
+
+public class FoodInfo {
+    private String foodname;
+    private String food_count;
+    private String food_price;
+
+    public String getFoodname() {
+        return foodname;
+    }
+
+    public void setFoodname(String foodname) {
+        this.foodname = foodname;
+    }
+
+    public String getFood_count() {
+        return food_count;
+    }
+
+    public void setFood_count(String food_count) {
+        this.food_count = food_count;
+    }
+
+    public String getFood_price() {
+        return food_price;
+    }
+
+    public void setFood_price(String food_price) {
+        this.food_price = food_price;
+    }
+}
